9f84130f00849352017a9ca0533347c72df3db6f
Fazle Rabby Talukder <talukderfazlerabby@gmail.com>
2024-05-02 11:08:15 +0600

    db connect in every call

--- db/queries.js
+++ db/queries.js
@@ -1,15 +1,18 @@
 import { userModel } from "@/models/user-model";
+import { dbConnect } from "@/services/mongo";
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-utils";
 import mongoose from "mongoose";
 
 const { recipeModel } = require("@/models/recipe-model");
 
 async function getAllRecipe() {
+    await dbConnect();
     const allRecepies = await recipeModel.find().lean();
     return replaceMongoIdInArray(allRecepies);
 }
 
 async function getRecipesByCategory(category) {
+    await dbConnect();
     try {
         const recipes = await recipeModel.find({ category }).lean();
         return replaceMongoIdInArray(recipes);
@@ -20,15 +23,18 @@ async function getRecipesByCategory(category) {
 }
 
 async function getRecipeById(recipeId) {
+    await dbConnect();
     const recipe = await recipeModel.findById(recipeId).lean();
     return replaceMongoIdInObject(recipe);
 }
 
 async function createUser(user) {
+    await dbConnect();
     return await userModel.create(user);
 }
 
 async function findUserByCredentials(credentials) {
+    await dbConnect();
     const user = await userModel.findOne(credentials).lean();
     if (user) {
         return replaceMongoIdInObject(user);
@@ -37,16 +43,19 @@ async function findUserByCredentials(credentials) {
 }
 
 async function getUserById(userId) {
+    await dbConnect();
     const user = await userModel.findById(userId).lean();
     return replaceMongoIdInObject(user);
 }
 
 async function getAllUser() {
+    await dbConnect();
     const allUser = await userModel.find().lean();
     return replaceMongoIdInArray(allUser);
 }
 
 async function updateFavourite(recipeId, authId) {
+    await dbConnect();
     // console.log(recipeId, authId);
 
     const user = await userModel.findById(authId);
